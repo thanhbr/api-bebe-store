3f95e07bc736629b6246b700b01705424039609f
ThanhBr <thanhmt>
2024-08-15 16:29:28 +0700

    add init user

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import userRouter from "./routes/users.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+app.use("/users", userRouter);
 app.get("/", (req,res) => {
     res.send("This is a response");
 })
