b92564174c0a5e4368ecff5a392a22a9bb976563
ThanhBr <thanhmt>
2024-08-20 08:56:11 +0700

    update api create brand

--- repositories/brand.js
+++ repositories/brand.js
@@ -2,13 +2,11 @@ import Exception from "../exceptions/Exception.js";
 import { OutputType, print } from "../helpers/print.js";
 import Brand from "../models/Brand.js";
 
-const create = async({ code, name, urlKey, logo }) => {
+const create = async ({ code, name, urlKey, logo }) => {
     try {
-        print(logo, OutputType.WARNING);
+        print(urlKey, OutputType.WARNING);
         const newBrand = await Brand.create({ code, name, urlKey, logo });
-        return {
-            ...newBrand._doc,
-        }
+        return { ...newBrand._doc }
     } catch (exception) {
         if(!!exception.errors) {
             throw new Exception(Exception.CANNOT_CREATE_BRAND, exception.errors);
